ec5a9e2b2cc060fc080d26b1db0791cc06cb701f
abhijith-bk <61800420+abhijith-bk@users.noreply.github.com>
2022-02-09 10:46:36 +0530

    Added project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
